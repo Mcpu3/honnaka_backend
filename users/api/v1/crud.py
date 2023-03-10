df2c59ea4318785f9f13494a19e7b304ea9e43db
岩本圭介 <mcpu3@outlook.jp>
2023-03-10 23:12:16 +0900

    セキュリティをテストするプログラムを追加 #3

---
+++ users/api/v1/crud.py
@@ -0,0 +1,134 @@
+from datetime import datetime
+import os
+from typing import Optional
+
+import pyodbc
+
+from users.api.v1.schema import User
+
+
+connection = pyodbc.connect("Driver={ODBC Driver 18 for SQL Server};Server=tcp:honnaka-backend.database.windows.net,1433;Database=honnaka-backend;Uid=iwamoto.keisuke629@honnaka-backend;Pwd={%s};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;" % os.getenv("PASSWORD"))
+
+def create_user(user: User):
+    user_uuid = user.user_uuid
+    user_name = user.user_name
+    hashed_password = user.hashed_password
+    created_at = user.created_at.strftime("%Y-%m-%d %H:%M:%S")
+    deleted = 0
+    with connection.cursor() as cursor:
+        cursor.execute(f"""
+        insert into users (
+            user_uuid,
+            user_name,
+            hashed_password,
+            created_at,
+            deleted
+        )
+        values (
+            '{user_uuid}',
+            '{user_name}',
+            '{hashed_password}',
+            '{created_at}',
+            {deleted}
+        )
+        """)
+
+def read_user(user_uuid: Optional[str] = None, user_name: Optional[str] = None) -> User:
+    user = None
+
+    if user_uuid:
+        user = read_user_by_user_uuid(user_uuid)
+    if user_name:
+        user = read_user_by_user_name(user_name)
+
+    return user
+
+def read_user_by_user_uuid(user_uuid: str) -> User:
+    user = None
+
+    with connection.cursor() as cursor:
+        cursor.execute(f"""
+            select
+                user_uuid,
+                user_name,
+                hashed_password,
+                display_name,
+                created_at,
+                updated_at,
+                deleted
+            from users
+            where 
+                user_uuid = '{user_uuid}' and
+                deleted = 0
+        """)
+        data = cursor.fetchone()
+        if data:
+            user = User(
+                user_uuid = data[0],
+                user_name = data[1],
+                hashed_password = data[2],
+                display_name = data[3],
+                created_at = data[4],
+                updated_at = data[5],
+                deleted = data[6]
+            )
+
+    return user
+
+def read_user_by_user_name(user_name: str) -> User:
+    user = None
+
+    with connection.cursor() as cursor:
+        cursor.execute(f"""
+            select
+                user_uuid,
+                user_name,
+                hashed_password,
+                display_name,
+                created_at,
+                updated_at,
+                deleted
+            from users
+            where 
+                user_name = '{user_name}' and
+                deleted = 0
+        """)
+        data = cursor.fetchone()
+        if data:
+            user = User(
+                user_uuid = data[0],
+                user_name = data[1],
+                hashed_password = data[2],
+                display_name = data[3],
+                created_at = data[4],
+                updated_at = data[5],
+                deleted = data[6]
+            )
+
+    return user
+
+def update_hashed_password(user_name: str, hashed_password: str, updated_at: datetime):
+    updated_at = updated_at.strftime("%Y-%m-%d %H:%M:%S")
+    with connection.cursor() as cursor:
+        cursor.execute(f"""
+            update users
+            set
+                hashed_password = '{hashed_password}',
+                updated_at = '{updated_at}'
+            where
+                user_name = '{user_name}' and
+                deleted = 0
+        """)
+
+def update_display_name(user_name: str, display_name: str, updated_at: datetime):
+    updated_at = updated_at.strftime("%Y-%m-%d %H:%M:%S")
+    with connection.cursor() as cursor:
+        cursor.execute(f"""
+            update users
+            set
+                display_name = '{display_name}',
+                updated_at = '{updated_at}'
+            where
+                user_name = '{user_name}' and
+                deleted = 0
+        """)
