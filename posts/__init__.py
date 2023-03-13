9c500ad51cb89362599743fecf11affe737f0c5f
岩本圭介 <mcpu3@outlook.jp>
2023-03-13 11:47:53 +0900

    設定を修正 #7

--- posts/__init__.py
+++ posts/__init__.py
@@ -1,24 +1,11 @@
-import logging
-
 import azure.functions as func
+from fastapi import FastAPI
+
+from posts.api.v1 import api
 
 
-def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+app = FastAPI()
+app.include_router(api.api_router, prefix="/api/v1")
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
+    return func.AsgiMiddleware(app).handle(req, context)
